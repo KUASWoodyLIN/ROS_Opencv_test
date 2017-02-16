552aa476d26d3d8cde65d5254b6c522e5bad0861
WoodyLin <jj7158761@gmail.com>
2017-02-16 23:16:38 +0800

    ros_opencv commit

--- VideoCapture/src/VideoCapture V2.cpp
+++ VideoCapture/src/VideoCapture V2.cpp
@@ -4,26 +4,27 @@
 #include <cv_bridge/cv_bridge.h>
 #include <iostream>
 
+#include <sensor_msgs/CompressedImage.h>
+
 // Subscriber to bottom camera
 image_transport::Subscriber sub;
 
 // Time control
 ros::Time lastTime;
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+void imageCallback(const sensor_msgs::CompressedImage::ImageConstPtr& msg)
 {
     try
     {
         // Get the msg image
         cv::Mat InImage;
-        InImage = cv_bridge::toCvShare(msg,"bgr8" )->image;
-//        InImage=cv_bridge::cvtColorForDisplay(cv_bridge::toCvShare(msg,"bgr8"))->imag$
+        InImage = cv_bridge::toCvShare(msg, "jpg")->image;
         cv::imshow("view", InImage);
-//        cv::waitKey(0);
+        cv::waitKey(30);
     }
     catch (cv_bridge::Exception& e)
     {
-        ROS_ERROR("Could not convert from '%s' to 'jpg'.", msg->encoding.c_str());
+        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
     }
 }
 
@@ -31,14 +32,8 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_listener");
     ros::NodeHandle nh;
-    cv::namedWindow("view",CV_WINDOW_NORMAL);
-    cv::startWindowThread();
-    lastTime = ros::Time::now();
     image_transport::ImageTransport it(nh);
-    sub = it.subscribe("camera/image", 1, imageCallback);
-   // sub = it.subscribe("camera/image", 1, imageCallback,ros::VoidPtr(),image_transpor$
+    sub = it.subscribe("camera/image/compressed", 1, imageCallback);
     ros::spin();
     cv::destroyWindow("view");
 }
-
-
--- VideoCapture/src/VideoCaptureV3.cpp
+++ VideoCapture/src/VideoCaptureV3.cpp
@@ -1,8 +1,13 @@
+  GNU nano 2.2.6            File: VideoCapture.cpp                              
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <compressed_image_transport/compressed_subscriber.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <iostream>
+
+#include <sensor_msgs/CompressedImage.h>
 
 // Subscriber to bottom camera
 image_transport::Subscriber sub;
@@ -10,20 +15,19 @@ image_transport::Subscriber sub;
 // Time control
 ros::Time lastTime;
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+void imageCallback(const sensor_msgs::CompressedImage::ConstPtr& msg)
 {
     try
     {
         // Get the msg image
         cv::Mat InImage;
-        InImage = cv_bridge::toCvShare(msg,"bgr8" )->image;
-//        InImage=cv_bridge::cvtColorForDisplay(cv_bridge::toCvShare(msg,"bgr8"))->imag$
+        InImage = cv_bridge::toCvShare(msg, "bgr8" )->image;
         cv::imshow("view", InImage);
-//        cv::waitKey(0);
+        cv::waitKey(30);
     }
     catch (cv_bridge::Exception& e)
     {
-        ROS_ERROR("Could not convert from '%s' to 'jpg'.", msg->encoding.c_str());
+        ROS_ERROR("Could not convert from '%s' to 'jpg'.", msg->encoding.c_str($
     }
 }
 
@@ -31,14 +35,14 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_listener");
     ros::NodeHandle nh;
-    cv::namedWindow("view",CV_WINDOW_NORMAL);
+    cv::namedWindow("view");
     cv::startWindowThread();
     lastTime = ros::Time::now();
     image_transport::ImageTransport it(nh);
-    sub = it.subscribe("camera/image", 1, imageCallback);
-   // sub = it.subscribe("camera/image", 1, imageCallback,ros::VoidPtr(),image_transpor$
+    sub = it.subscribe("camera/image/compressed", 1, imageCallback);
     ros::spin();
     cv::destroyWindow("view");
 }
+
 
 
